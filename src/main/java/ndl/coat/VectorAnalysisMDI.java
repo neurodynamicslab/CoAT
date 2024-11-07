89b12c154bef4eee6c15cc1bdcda0ef4c2d6dc57
NDL <theneurodynamicslab@gmail.com>
2024-11-07 14:45:19 +0530

    Fixed a bug that tried to retrieve the file name corresponding to empty row in file list.

--- src/main/java/ndl/coat/VectorAnalysisMDI.java
+++ src/main/java/ndl/coat/VectorAnalysisMDI.java
@@ -2171,8 +2171,7 @@ public class VectorAnalysisMDI extends javax.swing.JFrame implements ActionListe
             }
             var errorCount = errorlist.length;
             for(int a = 0 ;  a < errorCount ; a++){
-                this.setStatusMessage("Error reading the file#:" + this.rel2absPathMaps.get(errorlist[a])+"\n");
-                
+                this.setStatusMessage("Error reading the file#:" + errorlist[a] +"\n");  
             }
             grpName = (String)FileAssignmentTable.getValueAt(Count, 2);
             trialName = (String)FileAssignmentTable.getValueAt(Count,3);
